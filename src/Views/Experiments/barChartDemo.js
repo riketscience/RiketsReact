550f6b792989c41b217e5eec4157b95fcd1ac87e
riketscience <riketscience@gmail.com>
2020-05-10 19:21:27 +0100

    bar chart working provisionally

--- src/Views/Experiments/barChartDemo.js
+++ src/Views/Experiments/barChartDemo.js
@@ -3,38 +3,22 @@ import Slider from "./slider";
 import AngleSlider from "./angleSlider";
 
 var circleProps = {
-    title: "expansion factor",
-    points: [2,4,8,16,32,64],
-    pointsString: "2,4,6,8,32,64]",
-    factor: 2,
+    points: [1.78,2.67,4,6,9,13.5,20.25,30.375,45.56]
 }
 
-var angleProps = {
-    title: "angle",
-    yAngle: 0.75,
-}
-
-const svgWidth = 800;
 const xWidth = 800;
 const yHeight = 380;
 
-function BarChartDemo() {
+function BarchartDemo() {
 
-    const [factor, setFactor] = useState(4);
-    const [honey, setPoints] = useState(circleProps.points); 
-    const [angle, setAngle] = useState(angleProps.yAngle); 
+    const [factor, setFactor] = useState(2);
+    const [points, setPoints] = useState(circleProps.points); 
+    const [yAngle, setAngle] = useState(0.75); 
     
-    const xOffset = xWidth / 4, yOffset = 50, sizeFactor = 0.6, fillOpacity = 0.9;
+    const xOffset = 2, yOffset = 2, sizeFactor = 0.6, fillOpacity = 0.9;
 
-    circleProps.sliderUpdated = (newVal) => {
-        circleProps.factor = newVal;
-        setFactor(circleProps.factor);
-    }   
-
-    angleProps.angleUpdated = (newAngle) => {
-        setAngle(newAngle);
-        angleProps.yAngle = newAngle;
-    };
+    const expansionUpdated = (newVal) => { setFactor(newVal); }   
+    const angleUpdated = (newAngle) => { setAngle(newAngle); };
 
     function getMin() { 
         var min = circleProps.points.length > 0 ? circleProps.points[0] : 0;
@@ -70,49 +54,57 @@ function BarChartDemo() {
         circleProps.points = newPoints.filter(v => { return v !== ""; }).map(m => { return parseFloat(m); });
     };
 
-    function scaler(v) {
-        var colourScale = [0,255];
-        var sMin = colourScale[0];
-        var sMax = colourScale[1];
+    function colorScale(v) {
+        var min = 0;
+        var max = 250;
         var l = getMin();
         var h = getMax();
         var percentage = (v - l) / (h - l);
-        let scaleBand = (percentage * (sMax - sMin)) + sMin;
+        let scaleBand = (percentage * (max - min)) + min;
+        return scaleBand;
+    }
+
+    function heightScale(v, min, max) {
+        var l = getMin();
+        var h = getMax();
+        var percentage = (v - l) / (h - l);
+        let scaleBand = (percentage * (max - min)) + min;
         return scaleBand;
     }
 
     function color(c) {
-        var factor = scaler(c);
+        var factor = colorScale(c, 0, 255);
         var R =  factor;  // "Woody":(255 - factor); "Stinger": factor; "GreenTrees":(255 - factor) / 3;
         var G = factor / 1.5;
-        return "rgba("+(R)+","+G+","+0+","+fillOpacity+")";
+        var B = (255 - factor) / 2.5;
+        return "rgba("+(R)+","+G+","+B+","+fillOpacity+")";
     };
+
+    const svgWidth = 796;
+    const svgHeight = 476;
+
+    function getHeight(v) {
+        var max = getMax();
+        return svgHeight - (v * svgHeight / max);
+    }
 
     return (
         <div>
-            <input type="text" value={honey} onChange={e => (pointsListChanged(e.target.value))}/> 
+            <input type="text" value={points} onChange={e => (pointsListChanged(e.target.value))}/> 
             <div>
             <div>Min: {getMin()}</div>
             <div>Max: {getMax()}</div>
             <div>Mean: {getMean()}</div>
             </div>
             <div class="sliders">
-                <Slider {...circleProps} />
-                <AngleSlider {...angleProps} />
+                <Slider expansionUpdated={expansionUpdated} />
+                <AngleSlider angleUpdated={angleUpdated} />
             </div>
             <div class="svg-render">
                 <svg style={{width: 800, height: 480}}>
-                    {circleProps.points.map(val => (
-                        <circle fill={color(val)} r={val * sizeFactor * (1 + circleProps.factor / 8 )} cx={(val*circleProps.factor) + xOffset} cy={val*circleProps.factor * angleProps.yAngle + yOffset}></circle>
-                    ))}
-                    {circleProps.points.map(val => (
-                        <circle fill={color(val)} r={val * sizeFactor * (1 + circleProps.factor / 8 )} cx={xWidth - (val*circleProps.factor + xOffset)} cy={val*circleProps.factor * angleProps.yAngle + yOffset}></circle>
-                    ))}
-                    {circleProps.points.map(val => (
-                        <circle fill={color(val)} r={val * sizeFactor * (1 + circleProps.factor / 8 )} cx={val*circleProps.factor + xOffset} cy={yHeight - val*circleProps.factor * angleProps.yAngle + yOffset}></circle>
-                    ))}
-                    {circleProps.points.map(val => (
-                        <circle fill={color(val)} r={val * sizeFactor * (1 + circleProps.factor / 8 )} cx={xWidth - (val*circleProps.factor + xOffset)} cy={yHeight - val*circleProps.factor * angleProps.yAngle + yOffset}></circle>
+                    <rect stroke="#F00" fill="transparent" x="2" y="2" width={svgWidth} height={svgHeight}></rect>
+                    {circleProps.points.map((val, index) => (
+                        <rect fill={color(val)} x={xOffset + (index*svgWidth/circleProps.points.length)} y={getHeight(val)+ yOffset} width={svgWidth/circleProps.points.length} height={svgHeight - getHeight(val)}></rect>
                     ))}
                 </svg>
             </div>
@@ -120,4 +112,4 @@ function BarChartDemo() {
     )
 }
 
-export default BarChartDemo;
+export default BarchartDemo;
--- src/Views/Experiments/myTabs.js
+++ src/Views/Experiments/myTabs.js
@@ -22,7 +22,7 @@ function TabPanel(props) {
         {...other}
       >
         {value === index && (
-          <Box p={3}>
+          <Box>
             <Typography>{children}</Typography>
           </Box>
         )}
