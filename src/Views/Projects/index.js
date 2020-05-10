d99d515e68f1717e01c014f896d01df9c30b19f9
riketscience <riketscience@gmail.com>
2020-05-10 16:48:31 +0100

    profanity avoidance

--- src/Views/Projects/index.js
+++ src/Views/Projects/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Projects() {
     return (
         <div>
-            <h3>MY HEAD!!!</h3>
-            <p>I've done loads of shit, me...</p>
+            <h3>Audiovisual Portfolio</h3>
+            <p>Coming soon...</p>
         </div>
     );
 };
